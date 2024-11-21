eca85bb5dc65635caa9e8c5a6c2b02c802e1b29c
jinerfreya <149322974+jinerfreya@users.noreply.github.com>
2024-11-20 21:48:25 -0500

    image translation dao implemented

--- src/main/java/use_case/image_translation/ImageTranslationDataAccessInterface.java
+++ src/main/java/use_case/image_translation/ImageTranslationDataAccessInterface.java
@@ -2,7 +2,14 @@ package use_case.image_translation;
 import entity.User;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
-public class ImageTranslationDataAccessInterface {
+public abstract class ImageTranslationDataAccessInterface {
     void imageTranslation(User user, BufferedImage image) {}
+
+    public abstract void saveTranslation(String imageId, String originalText, String translatedText);
+
+    public abstract List<String> getTranslationHistory();
+
+    public abstract void deleteTranslation(String imageId);
 }
