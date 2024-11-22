15a7d7d49eee6fe2090e46b2ec524cb50ccc4388
haihai <qwertyuiop@gmail.com>
2024-11-21 21:26:48 -0500

    add attributes for CommonUser Entity for translation and chatbot features

--- src/main/java/entity/CommonUser.java
+++ src/main/java/entity/CommonUser.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.ArrayList;
+
 /**
  * A simple implementation of the User interface.
  */
@@ -7,6 +9,10 @@ public class CommonUser implements User {
 
     private final String name;
     private final String password;
+    private final String inputLan = "English";
+    private final String outputLan = "English";
+    private final ArrayList<ChatMessage> chatHistoryMessagesDisplay = new ArrayList<>();
+    private final ArrayList<ChatMessage> chatHistoryMessagesEng = new ArrayList<>();
 
     public CommonUser(String name, String password) {
         this.name = name;
@@ -23,4 +29,19 @@ public class CommonUser implements User {
         return password;
     }
 
+    public ArrayList<ChatMessage> getChatHistoryMessagesDisplay() {
+        return chatHistoryMessagesDisplay;
+    }
+
+    public ArrayList<ChatMessage> getChatHistoryMessagesEng() {
+        return chatHistoryMessagesEng;
+    }
+
+    public String getOutputLan() {
+        return outputLan;
+    }
+
+    public String getInputLan() {
+        return inputLan;
+    }
 }
