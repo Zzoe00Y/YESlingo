496c06ca5645e13603cb30093fcc27624e913296
Freya Zhang <149322974+freyazjiner@users.noreply.github.com>
2024-11-22 16:56:01 -0500

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,80 +1,145 @@
 package app;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
-import external_services.MyMemoryGateway;
+import entity.User;
+import entity.UserFactory;
 import external_services.ImageToTextAPIService;
+import external_services.MyMemoryGateway;
+import external_services.TextToTextTranslationService;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.chatbot.ChatBotViewModel;
 import interface_adapter.image_translation.ImageTranslationController;
 import interface_adapter.image_translation.ImageTranslationPresenter;
+import interface_adapter.loggedin_homepage.LoggedInController;
+import interface_adapter.loggedin_homepage.LoggedInPresenter;
+import interface_adapter.loggedin_homepage.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.profile.ProfileController;
+import interface_adapter.profile.ProfilePresenter;
+import interface_adapter.profile.ProfileViewModel;
+import interface_adapter.profile.change_language.ChangeLanguageController;
+import interface_adapter.profile.change_language.ChangeLanguagePresenter;
+import interface_adapter.profile.change_language.ChangeLanguageViewModel;
+import interface_adapter.profile.change_password.ChangePasswordController;
+import interface_adapter.profile.change_password.ChangePasswordPresenter;
+import interface_adapter.profile.change_password.ChangePasswordViewModel;
 import interface_adapter.text_translation.TextTranslationController;
 import interface_adapter.text_translation.TextTranslationPresenter;
 import use_case.image_translation.ImageTranslationInteractor;
 import use_case.image_translation.ImageTranslationOutputBoundary;
+import use_case.loggedin.LoggedInInputBoundary;
+import use_case.loggedin.LoggedInInteractor;
+import use_case.loggedin.LoggedInOutputBoundary;
+import use_case.loggedin.LoggedInUserDataAccessInterface;
+import use_case.profile.ProfileInteractor;
+import use_case.profile.ProfileOutputBoundary;
+import use_case.profile.ProfileUserDataAccessInterface;
+import use_case.profile.change_language.ChangeLanguageInputBoundary;
+import use_case.profile.change_language.ChangeLanguageInteractor;
+import use_case.profile.change_password.ChangePasswordInteractor;
+import use_case.profile.change_password.ChangePasswordUserDataAccessInterface;
+import use_case.text_translation.TextTranslationDataAccessInterface;
 import use_case.text_translation.TextTranslationInteractor;
 import use_case.text_translation.TextTranslationOutputBoundary;
-import use_case.text_translation.TextTranslationDataAccessInterface;
-import view.LoggedInView;
+import use_case.text_translation.TextTranslationUseCase;
+import view.*;
 
+/**
+ * The Main class of our application.
+ */
 public class Main {
+    /**
+     * Builds and runs the CA architecture of the application.
+     * @param args unused arguments
+     */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                // Initialize app builder
-                final AppBuilder appBuilder = new AppBuilder();
+        final AppBuilder appBuilder = new AppBuilder();
+        // add the Logout Use Case to the app using the appBuilder
+        final JFrame application = appBuilder
+                .addLoginView()
+                .addSignupView()
+                .addLoggedInView()
+                .addProfileView()
+                .addChangePasswordView()
+                .addChangeLanguageView()
+                .addChatBotView()
+                .addSignupUseCase()
+                .addLoginUseCase()
+//                                            .addLoggedinUseCase()
+                //.addProfileUseCase()
+                .addChatBotUseCase()
+//                                            .addChangePasswordUseCase()
+//                                            .addLogoutUseCase()
+                .build();
 
-                // Build basic application frame
-                final JFrame application = appBuilder
-                        .addLoginView()
-                        .addSignupView()
-                        .addLoggedInView()
-                        .addSignupUseCase()
-                        .addLoginUseCase()
-                        .addLogoutUseCase()
-                        .build();
+        application.pack();
+        application.setVisible(true);
 
-                // Get reference to LoggedInView
-                LoggedInView loggedInView = appBuilder.getLoggedInView();
+        // Get reference to LoggedInView
+        LoggedInView loggedInView = appBuilder.getLoggedInView();
 
-                // Initialize gateway and service for translation
-                TextTranslationDataAccessInterface textTranslationDataAccessInterface = new MyMemoryGateway();
+        // Initialize gateway and service for translation
+        TextTranslationDataAccessInterface textTranslationDataAccessInterface = new MyMemoryGateway();
 
-                // Set up text translation components
-                TextTranslationOutputBoundary textTranslationOutputBoundary =
-                        new TextTranslationPresenter(loggedInView);
+        // Set up text translation components
+        TextTranslationOutputBoundary textTranslationOutputBoundary =
+                new TextTranslationPresenter(loggedInView);
 
-                TextTranslationInteractor textTranslationInteractor =
-                        new TextTranslationInteractor(textTranslationDataAccessInterface, textTranslationOutputBoundary);
-                
-                TextTranslationController textTranslationController =
-                        new TextTranslationController(textTranslationInteractor);
+        TextTranslationInteractor textTranslationInteractor =
+                new TextTranslationInteractor(textTranslationDataAccessInterface, textTranslationOutputBoundary);
 
-                loggedInView.setTextTranslationController(textTranslationController);
+        TextTranslationController textTranslationController =
+                new TextTranslationController(textTranslationInteractor);
 
-                ImageToTextAPIService imageToTextService = new ImageToTextAPIService();
+        loggedInView.setTextTranslationController(textTranslationController);
 
-                ImageTranslationOutputBoundary imageTranslationOutputBoundary =
-                        new ImageTranslationPresenter(loggedInView);
+        ImageToTextAPIService imageToTextService = new ImageToTextAPIService();
+        ImageTranslationOutputBoundary imageTranslationOutputBoundary =
+                new ImageTranslationPresenter(loggedInView);
+        ImageTranslationInteractor imageTranslationInteractor =
+                new ImageTranslationInteractor(
+                        imageToTextService,
+                        textTranslationInteractor,
+                        imageTranslationOutputBoundary
+                );
+        ImageTranslationController imageTranslationController =
+                new ImageTranslationController(imageTranslationInteractor);
+        loggedInView.setImageTranslationController(imageTranslationController);
 
-                ImageTranslationInteractor imageTranslationInteractor =
-                        new ImageTranslationInteractor(
-                                imageToTextService,
-                                textTranslationInteractor,
-                                imageTranslationOutputBoundary
-                        );
 
-                ImageTranslationController imageTranslationController =
-                        new ImageTranslationController(imageTranslationInteractor);
+        ViewManagerModel viewManagerModel = appBuilder.getViewManagerModel();
+        LoggedInViewModel loggedInViewModel = appBuilder.getLoggedInViewModel();
+        ProfileViewModel profileViewModel = appBuilder.getProfileViewModel();
+        UserFactory userFactory = appBuilder.getUserFactory();
 
-                loggedInView.setImageTranslationController(imageTranslationController);
+        LoggedInOutputBoundary loggedInOutputBoundary = new LoggedInPresenter(viewManagerModel, loggedInViewModel, profileViewModel, new ChatBotViewModel());
+        LoggedInInteractor loggedInInteractor = new LoggedInInteractor(loggedInOutputBoundary, userFactory);
+        LoggedInController loggedInController = new LoggedInController(loggedInInteractor);
+        loggedInView.setLoggedInController(loggedInController);
 
-                // Make the frame visible
-                application.pack();
-                application.setVisible(true);
+        ProfileView profileView = appBuilder.getProfileView();
+        ChangePasswordViewModel changePasswordViewModel = appBuilder.getChangePasswordViewModel();
+        LoginViewModel loginViewModel = appBuilder.getLoginViewModel();
+        ChangeLanguageViewModel changeLanguageViewModel = appBuilder.getChangeLanguageViewModel();
+        ProfilePresenter profilePresenter = new ProfilePresenter(viewManagerModel, loggedInViewModel, profileViewModel,
+                changePasswordViewModel, loginViewModel, changeLanguageViewModel);
+        ProfileInteractor profileInteractor = new ProfileInteractor(profilePresenter, userFactory);
+        ProfileController profileController = new ProfileController(profileInteractor);
+        profileView.setProfileController(profileController);
 
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        });
+
+        ChangePasswordView changePasswordView = appBuilder.getChangePasswordView();
+        ChangePasswordPresenter changePasswordPresenter = new ChangePasswordPresenter(viewManagerModel, changePasswordViewModel, profileViewModel);
+        ChangePasswordInteractor changePasswordUseCaseInteractor = new ChangePasswordInteractor(changePasswordPresenter, userFactory);
+        ChangePasswordController changePasswordController = new ChangePasswordController(changePasswordUseCaseInteractor);
+        changePasswordView.setChangePasswordController(changePasswordController);
+
+
+        ChangeLanguageView changeLanguageView = appBuilder.getChangeLanguageView();
+        ChangeLanguagePresenter changeLanguagePresenter = new ChangeLanguagePresenter(viewManagerModel, changeLanguageViewModel, profileViewModel);
+        ChangeLanguageInteractor changeLanguageUseCaseInteractor = new ChangeLanguageInteractor(changeLanguagePresenter, userFactory);
+        ChangeLanguageController changeLanguageController = new ChangeLanguageController(changeLanguageUseCaseInteractor);
+        changeLanguageView.setChangeLanguageController(changeLanguageController);
     }
 }
