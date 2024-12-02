806dadee17366d0905427b1c157497ee0e6e73db
emily <yc.zhao@mail.utoronto.ca>
2024-12-01 20:01:24 -0500

    solve checkstyle problems

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -19,6 +19,10 @@ public interface User {
      */
     String getPassword();
 
+    /**
+     * Sets the password of the user.
+     * @param password the password
+     */
     void setPassword(String password);
 
     /**
@@ -27,21 +31,57 @@ public interface User {
      */
     String getOutputLan();
 
+    /**
+     * Sets the output language of the user.
+     * @param outputLan the output language
+     */
     void setOutputLan(String outputLan);
 
+    /**
+     * Returns the input language of the user.
+     * @return the input language of the user.
+     */
     String getInputLan();
 
+    /**
+     * Sets the input language of the user.
+     * @param inputLan the password
+     */
     void setInputLan(String inputLan);
 
+    /**
+     * Returns the display chat history of the user.
+     * @return the display chat history of the user.
+     */
     ArrayList<ChatMessage> getChatHistoryMessagesDisplay();
 
+    /**
+     * Sets the display chat history of the user.
+     * @param newMessagesDisplay the display chat history
+     */
     void addChatHistoryMessagesDisplay(ChatMessage newMessagesDisplay);
 
+    /**
+     * Returns the chat history of the user.
+     * @return the chat history of the user.
+     */
     ArrayList<ChatMessage> getChatHistoryMessagesEng();
 
+    /**
+     * Sets the chat history of the user.
+     * @param newMessagesEng the chat history
+     */
     void addChatHistoryMessagesEng(ChatMessage newMessagesEng);
 
+    /**
+     * Returns the history of the user.
+     * @return the history of the user.
+     */
     ArrayList<String> getHistory();
 
+    /**
+     * Sets the history of the user.
+     * @param history the history
+     */
     void setHistory(ArrayList<String> history);
 }
